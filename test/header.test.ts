e06e16b9ebedb86694c12dc5fc034a427c3870bd
Jarrod Seccombe <jarrod.seccombe@gmail.com>
2023-01-10 22:10:04 -0500

    test: convert tests to node-tap
    node-tap offers better typescript support, built-in assertions, mocking, coverage... it just makes
    sense

--- test/header.test.ts
+++ test/header.test.ts
@@ -10,17 +10,30 @@ t.afterEach(t => {
     t.context.message = null;
 });
 
-t.only('throws a ParserError if the segment is not an MSH segment', t => {
+t.test('throws a ParserError if the segment is not an MSH segment', t => {
     const segment = t.context.message.replace('MSH', 'PVI') as string;
-    t.throws(getMetadata(segment), ParserError);
+    t.throws(() => getMetadata(segment), new ParserError({
+        name: 'INVALID_SEGMENT',
+        message: 'Non-MSH segment found at segment index 0'
+    }));
     t.end();
 });
 
 t.test('generates message delimiters', t => {
+    const message = getMetadata(t.context.message);
+    t.equal(message.delimiters, {
+        field: '|',
+        component: '^',
+        repetition: '~',
+        escape: '\\',
+        subComponent: '&'
+    });
     t.end();
 });
 
 t.test('sets the message version', t => {
+    const message = getMetadata(t.context.message);
+    t.equal(message.version, '2.5');
     t.end();
 });
 
@@ -32,6 +45,14 @@ t.test('sets the event type and trigger event', t => {
     t.end();
 });
 
+t.test('sets the message timestamp', t => {
+    const message = getMetadata(t.context.message);
+    t.equal(message.timestamp, new Date());
+    t.end();
+});
+
 t.test('sets the unique message identifier', t => {
+    const message = getMetadata(t.context.message);
+    t.equal(message.id, '1817457');
     t.end();
 });
